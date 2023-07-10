94d9c10e03634ad05635adb5c420fd5744c878e7
andaeys <ekayudha.sulistianda@quipper.com>
2023-07-10 17:47:58 +0700

    init message 2

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app anda disini"})
-
+    # return jsonify({"Choo Choo": "Welcome to your Flask app anda disini"})
+    return jsonify({"message": "di sini ayah pepsi",
+                    "kucing": {
+                        "calico": ["pepsi", "cokelat", "tompey"],
+                        "oyen": ["momo"]
+                    }
+                    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
